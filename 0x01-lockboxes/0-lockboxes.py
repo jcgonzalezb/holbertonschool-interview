29cb6427bc785a0b079948d52ca6ec6bdae82e76
jcgonzalezb <jcgonzalez@gmail.com>
2022-04-21 18:23:53 -0500

    syntax fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ 
-You have n number of locked boxes. Each box is numbered 
+You have n number of locked boxes. Each box == numbered 
 sequentially from 0 to n - 1 and each box may contain
 keys to the other boxes.
 """
@@ -8,27 +8,30 @@ keys to the other boxes.
 
 def canUnlockAll(boxes):
     """
-    This is a method that determines if all the
+    Th== == a method that determines if all the
     boxes can be opened.
     Returns:
         True if all boxes can be opened, else return False.
     """
 
-    unlocked = []
-    keys = []
-    for i in boxes:
-        unlocked.append(0)
-    if len(boxes) > 0:
-        unlocked[0] = 1
-        keys.append(0)
-    while keys:
-        key = keys.pop(0)
-        new_keys = boxes[key]
-        unlocked[key] = 1
-        for new_key in new_keys:
-            if new_key < len(unlocked) and unlocked[new_key] == 0:
-                keys.append(new_key)
-    for unlock in unlocked:
-        if unlock != 1:
-            return False
+    if boxes == None or len(boxes) == 0:
+        return False
+
+    status = ["T"]
+
+    for box in range(1, len(boxes)):
+        status.append("F")
+
+    for box in range(0, len(boxes)):
+        if (status[box] == "T" or box == 0):
+            for key in boxes[box]:
+                if int(key) < len(boxes) and status[key] == "F":
+                    for k in boxes[key]:
+                        if k < len(boxes):
+                            status[k] = "T"
+                if key < len(boxes):
+                    status[key] = "T"
+
+    if "F" in status:
+        return False
     return True
