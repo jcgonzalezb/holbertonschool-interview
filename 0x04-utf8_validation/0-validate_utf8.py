1775003020be8538d50926804df82473f81bd562
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-09 17:02:23 -0500

    initial version

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This project module contains one method that determines if a
+given data set represents a valid UTF-8 encoding.
+"""
+
+
+
+def validUTF8(data):
+    """
+    This is a method that determines if a given data set
+    represents a valid UTF-8 encoding.
+        Returns: True if data is a valid UTF-8 encoding,
+        else return False.
+    """
