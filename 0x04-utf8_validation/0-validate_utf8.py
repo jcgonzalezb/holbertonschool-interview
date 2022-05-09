4758d907cc4d2329bc401e5eeba96cb262a16c28
jcgonzalezb <jcgonzalez@gmail.com>
2022-05-09 18:02:16 -0500

    shorter code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,9 +4,6 @@ This project module contains one method that determines if a
 given data set represents a valid UTF-8 encoding.
 """
 
-NUMBER_OF_BITS_PER_BLOCK = 8
-MAX_NUMBER_OF_ONES = 4
-
 
 def validUTF8(data):
     """
@@ -17,48 +14,13 @@ def validUTF8(data):
         Returns: True if data is a valid UTF-8 encoding,
         else return False.
     """
-    index = 0
-    while index < len(data):
-        number = data[index] & (2 ** 7)
-        number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-        if number == 0:  # single byte char
-            index += 1
-            continue
-
-        # validate multi-byte char
-        number_of_ones = 0
-        while True:  # get the number of significant ones
-            number = data[index] & (2 ** (7 - number_of_ones))
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - number_of_ones - 1)
-            if number == 1:
-                number_of_ones += 1
-            else:
-                break
-
-            if number_of_ones > MAX_NUMBER_OF_ONES:
-                return False  # too much ones per char sequence
-
-        if number_of_ones == 1:
-            return False  # there has to be at least 2 ones
-
-        # move on to check the next byte in a multi-byte char sequence
-        index += 1
-
-        # check for out of bounds and exit early
-        if index >= len(data) or index >= (index + number_of_ones - 1):
-            return False
-
-        # every next byte has to start with "10"
-        for i in range(index, index + number_of_ones - 1):
-            number = data[i]
-
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-            if number != 1:
+    successive_10 = 0
+    for b in data:
+        b = bin(b).replace('0b', '').rjust(8, '0')
+        if successive_10 != 0:
+            successive_10 -= 1
+            if not b.startswith('10'):
                 return False
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-            if number != 0:
-                return False
-
-            index += 1
-
+        elif b[0] == '1':
+            successive_10 = len(b.split('0')[0]) - 1
     return True
