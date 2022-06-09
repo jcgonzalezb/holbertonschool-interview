e8476acf2af3b4b21278a93170ab54c85da261ff
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-09 11:01:11 -0500

    initial version

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,8 +3,17 @@
 Given a pile of coins of different values, determine the fewest number of
 coins needed to meet a given amount total
 """
-def makeChange(coins, total):
-    for i in coins:
-        total_list += coins[i]
-    return total_list
 
+
+def makeChange(coins: list, total: int) -> int:
+    if total <= 0:
+        return 0
+    total_list = sum(coins)
+    if total_list > total:
+        return -1
+    new_coins = []
+    sorted_coins = coins.sort()
+    highest_value_coin = sorted_coins[-1]
+    if 2 * highest_value_coin > total:
+        new_coins.append(highest_value_coin)
+        sorted_coins.pop()
