28c770ad0f4a266b111c98c94a7eec670f1bc4b8
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-09 21:48:58 -0500

    comments fixed

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,13 +7,13 @@ coins needed to meet a given amount total
 
 def makeChange(coins: list, total: int) -> int:
     """
-    Function that  determine the fewest number of coins needed to meet
-    a given amount total.
+    Function that  determine the fewest number of coins needed
+    to meet a given amount total.
         Returns:
             Fewest number of coins needed to meet total.
             If total is 0 or less, return 0.
-            If total cannot be met by any number of coins you have, return -1.
-
+            If total cannot be met by any number of coins you have,
+            return -1.
     """
     if total <= 0:
         return 0
