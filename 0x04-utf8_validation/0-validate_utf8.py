957f0d188e88dc77eff02f1773d617e31f0dca86
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-11 12:43:44 -0500

    exception fixed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,13 +14,8 @@ def validUTF8(data):
         Returns: True if data is a valid UTF-8 encoding,
         else return False.
     """
-    successive_10 = 0
-    for b in data:
-        b = bin(b).replace('0b', '').rjust(8, '0')
-        if successive_10 != 0:
-            successive_10 -= 1
-            if not b.startswith('10'):
-                return False
-        elif b[0] == '1':
-            successive_10 = len(b.split('0')[0]) - 1
+    try:
+        bytes(data).decode()
+    except Exception:
+        return False
     return True
