e421a8a10aeac91e7239d69034e06d7fe1bdde18
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-16 20:48:35 -0500

    for fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,26 +6,17 @@ Task 0: Island Perimeter
 
 
 def island_perimeter(grid):
-    """
-    Returns the perimeter of the island, defined by grid
-    """
-    total = 0
-
-    rows = len(grid)
-    columns = len(grid[0])
-
-    for r in range(rows):
-        for c in range(columns):
-            cell = grid[r][c]
-            if cell is 1:
-                total += 4
-                if r != 0 and grid[r-1][c] is 1:
-                    total -= 1
-                if c != 0 and grid[r][c-1] is 1:
-                    total -= 1
-                if r + 1 != rows and grid[r + 1][c] is 1:
-                    total -= 1
-                if c + 1 != columns and grid[r][c + 1] is 1:
-                    total -= 1
-
-    return
+    """ Calculate perimeter of grid where "1" is found"""
+    p = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    p += 1  # top
+                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
+                    p += 1  # bottom
+                if col == 0 or grid[row][col - 1] == 0:
+                    p += 1  # left
+                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
+                    p += 1  # right
+    return p
